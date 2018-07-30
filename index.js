9c3d49e55919eb23e6d46bfa3e9342a84db1fdbd
Gil Fuser <gilfuser@gmail.com>
2018-07-30 17:01:09 -0300

    setup pusher

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ const errorHandler = require('errorhandler');
 const PORT = process.env.PORT || 5000;
 
 
+// --------------------------------------------------------------
+// SET UP PUSHER
+// --------------------------------------------------------------
+
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+});
+
+const pusherCallback = (err, req, res) => {
+  if (err) {
+    console.log('Pusher error:', err.message);
+    console.log(err.stack);
+  }
+};
+
+
 // ---------------------------------------------
 // SET UP EXPRESS
 // ---------------------------------------------
@@ -14,6 +35,21 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
+  .use((req, res, next) => { // simple logger
+    console.log('%s %s', req.method, req.url);
+    console.log(req.body);
+    next();
+  })
+  .use(errorHandler({
+    dumpExceptions: true,
+    showStack: true,
+  }))
+  .post('/message', (req, res) => {
+  // TODO: Check for valid POST data
+    const { socketId, channel, message } = req.body;
+    pusher.trigger(channel, 'message', message, socketId, pusherCallback);
+    res.send(200);
+  })
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
   // .get('/', (req, res) => res.render('pages/index'))
