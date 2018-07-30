e1d71eebd6dbc0f0ef4a3bd683d76e163fdbf82b
Gil Fuser <gilfuser@gmail.com>
2018-07-30 13:06:46 -0300

    comment out ejs stuff in express, include index.html

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const errorHandler = require('errorhandler');
+
+const PORT = process.env.PORT || 5000;
+
+
+// ---------------------------------------------
+// SET UP EXPRESS
+// ---------------------------------------------
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
